45d40133eec1eb199080bd8622123c66ea23a236
jwolniewicz <79764581+jwdeveloper@users.noreply.github.com>
2021-12-28 02:07:50 +0100

    Simple hello world

--- src/main/java/jw/fancy_fishing/Main.java
+++ src/main/java/jw/fancy_fishing/Main.java
@@ -1,13 +1,14 @@
 package jw.fancy_fishing;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN+"Hello world");
     }
 
     @Override
